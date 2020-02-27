f21cb6e5bfe81bbaf035f247f41d6c1216c75d31
olekfur <olekfur@gmail.com>
2020-02-27 15:22:40 +0100

    add c baseline

--- Tasks/2.py
+++ Tasks/2.py
@@ -1,18 +1,25 @@
 import findspark
-findspark.init("/home/fordiboii/spark")
+findspark.init("/home/olekfur/spark")
 
 from pyspark import SparkContext, SparkConf
 
-reviewerspath = "./data/yelp_top_reviewers_with_reviews.csv"
+reviewerspath = "../data/yelp_top_reviewers_with_reviews.csv"
 
 sparkConf = SparkConf().setAppName("Yelp").setMaster("local")
 sc = SparkContext(conf = sparkConf)
 
-"""2 a) How many distinct users are there in the dataset """
+# 2 a) How many distinct users are there in the dataset
 def distinctUsers(rdd):
     newRdd = rdd.map(lambda x: x.split()[1]).distinct()
     return newRdd.count()
 
+# 2 c) What is the business_id of the top 10 businesses with the most reviews
+def distinctBusinesses(rdd):
+    newRdd = rdd.map(lambda x: x.split()[2]).distinct()
+    return newRdd.count()
+
 reviewersTextFile = sc.textFile(reviewerspath)
 
-print(distinctUsers(reviewersTextFile))
+print("distinct users: " + str(distinctUsers(reviewersTextFile)))
+
+print("distinct businesses: " + str(distinctBusinesses(reviewersTextFile)))
