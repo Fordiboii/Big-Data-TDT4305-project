2c9c104547e05f446b9c6e935669ab832649a1eb
olekfur <olekfur@gmail.com>
2020-03-05 20:06:43 +0100

    add 3b to main script and write to file

--- Tasks/3.py
+++ Tasks/3.py
@@ -21,9 +21,25 @@ def averageCityRating():
     avgRatings = citiesAndRatings.aggregateByKey((0, 0), lambda  a,b:(a[0] + b, a[1]+ 1), lambda a,b: (a[0] + b[0], a[1] + b[1])).mapValues(lambda v: str(round(v[0]/v[1], 1)))
     lines = avgRatings.map(toCSVLine)
     lines.saveAsTextFile("output3a")
-    print(avgRatings.collect())
+    return avgRatings
+
+# Subtask b) What are the top 10 most frequent categories in the data?
+def mostFrequentCategories():
+    textFile = sc.textFile(businessesPath)
+    headers = textFile.first()
+    textFileWithoutHeaders = textFile.filter(lambda line: line != headers)
+    arrayOfCategories = textFileWithoutHeaders.map(lambda line: line.split("\t")[10:])
+    listOfCategoriesUnix = arrayOfCategories.flatMap(lambda list: list)
+    arrayOfCategoriesAscii = listOfCategoriesUnix.map(lambda category: str(category)).map(lambda category: category.split(","))
+    listOfCategoriesAscii = arrayOfCategoriesAscii.flatMap(lambda list: list)
+    categoryFrequencies = listOfCategoriesAscii.map(lambda category: (category, 1)).reduceByKey(lambda a,b: a+b)
+    topTenCategories = categoryFrequencies.takeOrdered(10, key = lambda x: -x[1])
+    lines = sc.parallelize(topTenCategories)
+    lines.saveAsTextFile("output3b")
+    return topTenCategories
 
 
+# Subtask c)
 def postalCodeCentroids():
     textFile = sc.textFile(businessesPath)
     headers = textFile.first()
@@ -36,8 +52,7 @@ def postalCodeCentroids():
     centroids = avgLat.join(avgLong)
     lines = centroids.map(tuplesToCSVLine)
     lines.saveAsTextFile("output3c")
-
-    print(centroids.collect())
+    return centroids
 
     
 def postalCodeLat(rdd):
@@ -59,6 +74,11 @@ def tuplesToCSVLine(data):
 def main():
     print("-------- (3a) Average business-ratings for each city: --------")
     averageCityRating()
+
+    print("-------- (3b) Top 10 most frequent categories: --------")
+    categories = mostFrequentCategories()
+    for category in categories:
+        print(category[0], category[1])
 
     print("-------- (3c) Geographical centroids for businesses for each postal code: --------")
     postalCodeCentroids()
